ee3a83690578388265853cce87cd8a3142bdb755
TimurKhan001 <timurkhan@seznam.cz>
2023-06-10 21:17:56 +0200

    preload changed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,52 +12,52 @@ class MyDocument extends Document {
 				<Head>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/p1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/p1.avif"
 						as="image"
 					/>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/s1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/s1.avif"
 						as="image"
 					/>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/k1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/k1.avif"
 						as="image"
 					/>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/m1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/m1.avif"
 						as="image"
 					/>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/sl1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/sl1.avif"
 						as="image"
 					/>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/b1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/b1.avif"
 						as="image"
 					/>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/o1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/o1.avif"
 						as="image"
 					/>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/a1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/a1.avif"
 						as="image"
 					/>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/n1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/n1.avif"
 						as="image"
 					/>
 					<link
 						rel="preload"
-						href="https://khan-project.s3.eu-central-1.amazonaws.com/l1.jpg"
+						href="https://khan-project.s3.eu-central-1.amazonaws.com/l1.avif"
 						as="image"
 					/>
 				</Head>
